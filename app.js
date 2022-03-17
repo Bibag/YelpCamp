9061903949757d47f6619a4c823ca208a2f218ec
Dat Tran <tiendat0274@gmail.com>
2022-03-17 18:13:19 +0700

    fix bug

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 const userRoutes = require('./routes/users');
 //Connect to mongo database
-const dbUrl = process.env.BD_URL || 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 mongoose.connect(dbUrl)
     .then(() => console.log('Database connected!'))
     .catch(error => console.log('Database connect error!', error));
